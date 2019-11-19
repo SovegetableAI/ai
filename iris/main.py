d35f4efd5e958bc90ba1ab6de4faf2f3fcddb33c
aodethri <740950342@qq.com>
2019-11-19 18:32:48 +0800

    iris

---
+++ iris/main.py
@@ -0,0 +1,36 @@
+import numpy as np
+import sklearn as sk
+import matplotlib.pyplot as plt
+import pathlib
+
+from sklearn.datasets import load_iris
+from NeuralNet import *
+
+fn = "iris.npz"
+
+def pretreatment():
+    x,y = load_iris(True)
+    y=np.reshape(y,(150,1))
+    np.savez('iris.npz',data=x,label=y)
+
+
+if __name__ == '__main__':
+    path = pathlib.Path('iris.npz')
+    if path.exists():
+        pass
+    else:
+        pretreatment()
+    nc = 3
+    reader = DataReader(fn)
+    reader.ReadData()
+    reader.NormalizeX()
+    reader.ToOneHot(nc, base=1)
+
+
+    ni = 4
+    params = HyperParameters(ni, nc, eta=0.15, max_epoch=150, batch_size=5, eps=1e-3, net_type=NetType.MultipleClassifier)
+    net = NeuralNet_1_2(params)
+    net.train(reader, checkpoint=1)
+
+
+    pass
